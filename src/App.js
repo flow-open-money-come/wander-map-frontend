000937a4629094707e38f9fe9987a74e710cf54a
謝文瑜 <wenyu@xiewenyudeMacBook-Air.local>
2021-10-02 15:27:58 +0800

    [login-register] basic layout (withouut third-party forms)

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ function App() {
           <Route path='/articles/id'></Route>
           <Route path='/trails/id'></Route>
         </Switch>
-        <Footer />
+        {/* <Footer /> */}
       </Router>
     </>
   )
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,13 +1,127 @@
-import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+import { COLOR, FONT, EFFECT, RADIUS, MEDIA_QUERY } from '../../constants/style'
 
+const RegisterPageWrapper = styled.div`
+  width: 100%;
+  background-image: linear-gradient(
+      to bottom,
+      rgba(0, 0, 0, 0.2) 0%,
+      rgba(0, 0, 0, 0.2) 100%
+    ),
+    url('https://i.imgur.com/Y5790Kx.png');
+  padding: 40px 0px;
+`
+const RegisterFormsWrapper = styled.div`
+  width: 80%;
+  margin: 0 auto;
+`
+const Title = styled.div`
+  font-size: ${FONT.lg};
+  color: ${COLOR.white};
+  text-align: center;
+  margin-bottom: 30px;
+  ${MEDIA_QUERY.lg} {
+    font-size: ${FONT.logo};
+  }
+`
 
+const Input = styled.input`
+  width: 250px;
+  height: 40px;
+  border-radius: ${RADIUS.lg};
+  margin-top: 20px;
+  background-color: rgba(0, 0, 0, 0.3);
+  border: none;
+  padding: 20px;
+  transition: ${EFFECT.transition};
+  font-size: ${FONT.s};
+  color: ${COLOR.white};
+  box-shadow: ${EFFECT.shadow_light};
+  &:hover {
+    width: 330px;
+    cursor: pointer;
+  }
+  &:focus {
+    width: 330px;
+    background-color: rgba(0, 0, 0, 0.6);
+    outline: none;
+  }
+  &::placeholder {
+    color: ${COLOR.white};
+    text-align: center;
+  }
+  ${MEDIA_QUERY.lg} {
+    font-size: ${FONT.md};
+    width: 350px;
+    &:hover {
+      width: 430px;
+      cursor: pointer;
+    }
+    &:focus {
+      width: 430px;
+      outline: none;
+    }
+  }
+`
 
-function LoginPage() {
-  
+const FormWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const SubmitBtn = styled.div`
+  width: 250px;
+  height: 40px;
+  padding: 10px 20px;
+  border: 1px solid ${COLOR.gray};
+  border-radius: ${RADIUS.lg};
+  margin-top: 25px;
+  font-size: 16px;
+  color: ${COLOR.white};
+  text-align: center;
+  transition: 0.5s linear;
+  font-size: ${FONT.s};
+  &:hover {
+    cursor: pointer;
+    background-color: ${COLOR.green};
+    color: white;
+  }
+  ${MEDIA_QUERY.lg} {
+    width: 350px;
+    font-size: ${FONT.md};
+  }
+`
+const AlertMsg = styled.span`
+  margin-top: 8px;
+  font-size: ${FONT.xs};
+  color: ${COLOR.white};
+  ${(props) =>
+    props.$error && `color: red; font-weight:bold; font-size:${FONT.s}`}
+`
+const OuterLink = styled(Link)`
+  font-size: ${FONT.xs};
+  font-weight: bold;
+  color: ${COLOR.white};
+`
+export default function LoginPage() {
   return (
-   <div>登入</div>
+    <>
+      <RegisterPageWrapper>
+        <Title> 會員登入 </Title>
+        <RegisterFormsWrapper>
+          <FormWrapper>
+            <AlertMsg $error>資料不齊全，請再次檢查。</AlertMsg>
+            <Input placeholder='帳號' />
+            <Input type='password' placeholder='密碼' />
+            <SubmitBtn> 登入 </SubmitBtn>
+            <AlertMsg>
+              還不是會員？<OuterLink>註冊</OuterLink>
+            </AlertMsg>
+          </FormWrapper>
+        </RegisterFormsWrapper>
+      </RegisterPageWrapper>
+    </>
   )
 }
-
-export default LoginPage;
