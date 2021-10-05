cfbbda9a0fe04d2f3735e58d9958bec20838da74
Yi-De <yidetseng@Yi-Dede-MacBook-Pro.local>
2021-10-05 21:52:38 +0800

    [develop] fixing component-Filter

--- src/components/forumSystem/Filter.js
+++ src/components/forumSystem/Filter.js
@@ -32,42 +32,6 @@ const Filter = styled.div`
   }
 `
 
-const UpperPart = styled.div`
-  display: flex;
-  justify-content: space-between;
-`
-
-const SearchInput = styled.input`
-  padding-left: 10px;
-  width: 90%;
-  border: 0;
-  border-bottom: 1px solid ${COLOR.beige};
-  background-color: ${COLOR.white};
-  color: ${COLOR.gray};
-
-  ${MEDIA_QUERY.md} {
-    font-size: ${FONT.md};
-  }
-
-  ${MEDIA_QUERY.lg} {
-    font-size: ${FONT.lg};
-  }
-`
-
-const SearchIcon = styled.button`
-  background-image: url('https://i.imgur.com/XG5YxVh.png');
-  background-size: contain;
-  border-bottom: 1px solid ${COLOR.beige};
-  width: 30px;
-  height: 30px;
-  cursor: pointer;
-
-  ${MEDIA_QUERY.md} {
-    width: 50px;
-    height: 50px;
-  }
-`
-
 const FilterTags = styled.div`
   margin-top: 10px;
   display: flex;
