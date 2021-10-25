866ee11b2d5306d3a96d726bd5625c49b228edb5
謝文瑜 <wenyu@xiewenyudeMacBook-Air.local>
2021-10-25 17:48:07 +0800

    footer add source code link, add backtotop btn opacity

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,25 +1,59 @@
 import styled from 'styled-components'
-import { COLOR, FONT } from '../../constants/style'
+import { COLOR, EFFECT, FONT, MEDIA_QUERY, RADIUS } from '../../constants/style'
+import { ReactComponent as GitHubIcon } from '../../icons/GitHub.svg'
 
 const FooterContainer = styled.footer`
   width: 100%;
-  max-height: 60px;
-  padding: 0px 10px;
   background-color: ${COLOR.green};
   color: ${COLOR.white};
   font-size: ${FONT.s};
+  line-height: 1.5rem;
+  padding: 10px;
+  position: relative;
+  left: 0px;
+  bottom: 0px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   text-align: center;
-  line-height: 30px;
-  position: fixed;
-  bottom: 0;
-  margin-top: 20px;
+  ${MEDIA_QUERY.lg} {
+    flex-direction: row;
+  }
 `
+
+const GitHubLink = styled.a`
+  text-decoration: none;
+  color: ${COLOR.beige};
+  padding: 2px 5px;
+  margin: 10px 0px 0px 10px;
+  transition: ${EFFECT.transition};
+  border: 1px solid ${COLOR.white};
+  border-radius: ${RADIUS.s};
+  display: flex;
+  align-items: center;
+
+  & svg {
+    margin-right: 10px;
+  }
+  &:hover {
+    color: ${COLOR.white};
+    background-color: ${COLOR.green_light};
+  }
+  ${MEDIA_QUERY.lg} {
+    margin: 0px 0px 0px 10px;
+  }
+`
+
 export default function Footer() {
   return (
     <>
       <FooterContainer>
         Copyright © 2021 Flow Open Money Come All Rights Reserved.
-        花開富貴股份有限公司版權所有
+        花開富貴股份有限公司版權所有。
+        <GitHubLink href='https://github.com/flow-open-money-come'>
+          <GitHubIcon /> Wander Map Source code.
+        </GitHubLink>
       </FooterContainer>
     </>
   )
