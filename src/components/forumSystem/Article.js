d7641104e2d6ecec4d3093f378b79eb9fab0da2f
Yi-De <yidetseng@Yi-Dede-MacBook-Pro.local>
2021-11-03 22:15:49 +0800

    [debug-der] removed memo from article

--- src/components/forumSystem/Article.js
+++ src/components/forumSystem/Article.js
@@ -1,4 +1,4 @@
-import React, { useContext, memo } from 'react'
+import React, { useContext } from 'react'
 import styled from 'styled-components'
 import { FONT, COLOR, RADIUS, MEDIA_QUERY } from '../../constants/style'
 import { Link } from 'react-router-dom'
@@ -209,4 +209,4 @@ function ArticleList({
   )
 }
 
-export default memo(ArticleList)
+export default ArticleList
